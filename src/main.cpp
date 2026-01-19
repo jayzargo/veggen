4f3600c15129221c294f264e9cbe8e24203e9622
jayzargo <matt.grnac@gmail.com>
2026-01-19 19:22:43 +0100

    cmake modifications

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,6 @@
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
-
 int main() {
     // GLM Test
     glm::vec3 position(1.0f, 2.0f, 3.0f);
