f38ccb11e20a2727f919aba8dbf0f13496a71c13
jayzargo <matt.grnac@gmail.com>
2026-01-26 16:21:04 +0100

    camera rotation

--- src/main.cpp
+++ src/main.cpp
@@ -241,8 +241,16 @@ int main() {
         glm::mat4 projection = glm::mat4(1.0f);
         
         model = glm::scale(model, glm::vec3(0.5f));
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        
+        const float radius = 3.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
+        //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -7.0f));
+        
         projection = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 100.0f);
 
         glm::mat4 mvp = projection * view * model;
